a27b4eea6ca5c15320fe94193f9b84942e4c332f
rflluc <132058513+rflluc@users.noreply.github.com>
2024-05-23 16:25:04 +0200

    Add files via upload

---
+++ llicencies.java
@@ -0,0 +1,9 @@
+public class llicencies {
+    public static void main(String[] args) {
+        String nom = "Lluc";
+        String assignatura = "Sistemes Informàtics-Llicències";
+      
+            System.out.println("El meu nom es: " + nom + " i estic fent " + "Estic fent una practica de " + assignatura  );
+        
+    }
+}
